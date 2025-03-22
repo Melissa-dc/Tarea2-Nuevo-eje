ffb3aea1b6e45f27bf14bcbe73c705f746a1aa62
Melissa <64035584+Melissa-dc@users.noreply.github.com>
2025-03-22 09:21:29 -0400

    Add files via upload

---
+++ Tarea2U3Deje/Tarea2U3Deje/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace OpenTK_U
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new OpenTK.Mathematics.Vector2i(800, 600),
+                Title = "Letter U 3D",
+            };
+
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+        }
+    }
+}
